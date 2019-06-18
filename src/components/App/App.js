604a2e007b3e22d85dc7fecf411c63c7f09e16d8
SHANNON MORANETZ <shannonrmoranetz@gmail.com>
2019-06-18 16:42:30 -0600

    Add header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Shannon Moranetz</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Header() {
+  return (
+    <div className="Header">
+      <Typography variant="h1" align="center">Shannon Moranetz</Typography>
+    </div>
+  );
+}
+
+export default Header;
